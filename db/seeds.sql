437d0f965cdd08f08ca82bdd055c64b9679be8f5
renee <reneebruff@gmail.com>
2018-05-07 18:02:39 -0400

    seeds file complete

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (name) VALUES ('Bacon Cheeseburger');
+INSERT INTO burgers (name) VALUES ('Jersey Burger');
+INSERT INTO burgers (name, eaten) VALUES ('Bleu Cheese Burger', true);
+INSERT INTO burgers (name, eaten) VALUES ('Veggie Burger', true);
+INSERT INTO burgers (name, eaten) VALUES ('Jalapeno Burger', true);
+INSERT INTO burgers (name) VALUES ('Turkey Burger');
